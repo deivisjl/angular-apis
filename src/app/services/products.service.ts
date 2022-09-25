fb29b3df7c73ce9644c55ad9654c3986544879ca
deivisjl <deivisjl@gmail.com>
2022-09-25 17:26:39 -0600

    Retry observable

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { CreateProductDTO, Product, UpdateProductDTO } from './../models/product.model';
+import { retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -21,7 +22,10 @@ export class ProductsService {
        params = params.set('limit',limit);
        params = params.set('offset',offset);
     }
-    return this.http.get<Product[]>(this.apiUrl,{ params });
+    return this.http.get<Product[]>(this.apiUrl,{ params })
+    .pipe(
+      retry(3)
+    );
   }
 
   getProduct(id: string){
