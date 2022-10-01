a46f9690ad6c0e137ed23b4a159a066cdea7eb5f
deivisjl <deivisjl@gmail.com>
2022-10-01 12:06:53 -0600

    Tranformaciones
    Se debe transformar en el servicio

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpParams, HttpStatusCode } from '@angular/common/http';
 
 import { CreateProductDTO, Product, UpdateProductDTO } from './../models/product.model';
-import { catchError, retry, throwError } from 'rxjs';
+import { catchError, retry, throwError, map } from 'rxjs';
 
 import { environment } from './../../environments/environment';
 
@@ -26,7 +26,13 @@ export class ProductsService {
     }
     return this.http.get<Product[]>(this.apiUrl,{ params })
     .pipe(
-      retry(3)
+      retry(3),
+      map(products => products.map(item=>{
+        return {
+          ...item,
+          taxes: .12 * item.price
+        }
+      }))
     );
   }
 
