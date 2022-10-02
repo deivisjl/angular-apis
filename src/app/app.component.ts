6b12c1b14b92ace3756b2b931a009071e7cc65d5
deivisjl <deivisjl@gmail.com>
2022-10-02 12:57:38 -0600

    Register and login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { Product } from './models/product.model';
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +13,33 @@ export class AppComponent {
   imgParent = '';
   showImg = true;
 
-
+  constructor(
+    private AuthService: AuthService,
+    private UsersService: UsersService
+  ){}
   onLoaded(img: string) {
     console.log('log padre', img);
   }
 
   toggleImg() {
     this.showImg = !this.showImg;
+  }
+
+  createUser(){
+    this.UsersService.create({
+      name: 'Sebas',
+      email: 'sebas2@gmail.com',
+      password:'12345'
+    })
+    .subscribe(rta =>{
+      console.log(rta)
+    });
+  }
+
+  login(){
+    this.AuthService.login('sebas2@gmail.com','12345')
+    .subscribe(rta =>{
+      console.log(rta)
+    });
   }
 }
